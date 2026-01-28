6fdc4290a53083ac6171dcb29ae0d07ec67ae097
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2026-01-28 06:27:10 +0200

    adding your groups row list view

--- lib/common/functions/custom_square_button.dart
+++ lib/common/functions/custom_square_button.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CustomSquareButton extends StatelessWidget {
-  final IconData icon;
+  final IconData? icon;
   final String label;
   final VoidCallback onTap;
   final Color? backgroundColor;
   final Color? borderColor;
-
   final Color? iconColor;
   final Color? textColor;
-
   final CrossAxisAlignment alignment;
+  final bool isExpanded;
+
   const CustomSquareButton({
     super.key,
-    required this.icon,
+    this.icon,
     required this.label,
     required this.onTap,
     this.backgroundColor,
@@ -21,50 +21,53 @@ class CustomSquareButton extends StatelessWidget {
     this.iconColor,
     this.textColor,
     this.alignment = CrossAxisAlignment.center,
+    this.isExpanded = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Material(
-        color: backgroundColor ?? Theme.of(context).scaffoldBackgroundColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-          side: borderColor != null
-              ? BorderSide(color: borderColor!, width: 0.5)
-              : BorderSide.none,
-        ),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(12),
-          onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
-            child: Column(
-              crossAxisAlignment: alignment,
-              mainAxisSize: MainAxisSize.min,
-              children: [
+    Widget buttonContent = Material(
+      color: backgroundColor ?? Theme.of(context).scaffoldBackgroundColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+        side: borderColor != null
+            ? BorderSide(color: borderColor!, width: 0.5)
+            : BorderSide.none,
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+          child: Column(
+            crossAxisAlignment: alignment,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (icon != null) ...[
                 Icon(
                   icon,
                   color: iconColor ?? Theme.of(context).iconTheme.color,
                   size: 24,
                 ),
                 const SizedBox(height: 6),
-                Text(
-                  label,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color:
-                        textColor ??
-                        Theme.of(context).textTheme.bodyMedium?.color,
-                  ),
-                ),
               ],
-            ),
+              Text(
+                label,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color:
+                      textColor ??
+                      Theme.of(context).textTheme.bodyMedium?.color,
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
+
+    return isExpanded ? Expanded(child: buttonContent) : buttonContent;
   }
 }
--- lib/presentation/groups/groups_view.dart
+++ lib/presentation/groups/groups_view.dart
@@ -1,5 +1,8 @@
+import 'package:auth/common/functions/custom_square_button.dart';
+import 'package:auth/core/styels.dart';
 import 'package:auth/presentation/groups/widgets/groups_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class GroupsView extends StatelessWidget {
   const GroupsView({super.key});
@@ -8,8 +11,68 @@ class GroupsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const GroupsAppBar(),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Your groups", style: Styles.textStyle18,),
+                CustomSquareButton(
+                  label: "See all",
+                  isExpanded: false,
+                  textColor: Colors.blue,
+                  onTap: () => context.push('/your_groups'),
 
-      body: Center(child: Text('Groups')),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 12),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 16),
+              itemCount: 12,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Column(
+                    children: [
+                      //TODO Replace with group image
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundColor: Colors.grey[200],
+                        child: const Icon(
+                          Icons.groups,
+                          color: Colors.grey,
+                          size: 30,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const SizedBox(
+                        width: 70,
+                        //TODO Replace with group name
+                        child: Text(
+                          "Group Nameeeeee",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 12,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
